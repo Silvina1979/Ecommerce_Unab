39f9f35c8d2a4ea0696975bec302dfd4a6bc0348
Silvina <silvinaglew@gmail.com>
2025-10-29 17:20:29 -0300

    Arreglos iniciales

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,2 +1,7 @@
-429: Too Many Requests
-For more on scraping GitHub and how it may affect your rights, please review our Terms of Service (https://docs.github.com/en/site-policy/github-terms/github-terms-of-service).
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
