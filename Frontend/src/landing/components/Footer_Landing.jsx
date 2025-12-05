2f622cd82a4de24b0313bb337b6126ddfeb4c56f
Silvina <silvinaglew@gmail.com>
2025-12-04 21:27:00 -0300

    mal escrito- footer..css

--- Frontend/src/landing/components/Footer_Landing.jsx
+++ Frontend/src/landing/components/Footer_Landing.jsx
@@ -1,5 +1,5 @@
 import { Link, useLocation } from "react-router-dom";
-import "../../styles/footer_landing.css";
+import "../../styles/Footer_Landing.css";
 
 import { LuExternalLink } from "react-icons/lu";
 /**
