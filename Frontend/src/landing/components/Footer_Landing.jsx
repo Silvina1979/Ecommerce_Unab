ea7fc12239a36c2d0cff553f040ed51702256d91
Silvina <silvinaglew@gmail.com>
2025-12-04 21:22:24 -0300

    prueba ref footer

--- Frontend/src/landing/components/Footer_Landing.jsx
+++ Frontend/src/landing/components/Footer_Landing.jsx
@@ -1,5 +1,5 @@
 import { Link, useLocation } from "react-router-dom";
-import "../styles/footer_landing.css";
+import "../../styles/footer_landing.css";
 
 import { LuExternalLink } from "react-icons/lu";
 /**
